cd2bc4c885af0c04b063936e90b93d2b20f0c563
Andreas Ekeroot <andeke@gmail.com>
2017-12-05 08:54:43 +0100

    Solution for day 5.

---
+++ 5/Main.hs
@@ -0,0 +1,29 @@
+module Main where
+
+import System.IO
+import Data.Vector (Vector)
+import qualified Data.Vector.Mutable as M
+import qualified Data.Vector as V
+
+run :: Int        -- ^ Number of jumps
+    -> Int        -- ^ Current position
+    -> Vector Int -- ^ Instructions
+    -> Int        -- ^ Number of jumps
+run n i v = case v  V.!? i of
+              Nothing -> n
+              Just i' ->
+                run
+                  (n + 1)
+                  (i + i')
+                  (V.modify (\v -> M.modify v go i) v)
+  where
+    -- For solution one: go = (+1)
+    go :: Int -> Int
+    go a
+      | a >= 3    = a - 1
+      | otherwise = a + 1
+
+main :: IO ()
+main = do
+  n <- run 0 0 . V.fromList . map read . lines <$> readFile "input.txt"
+  print n
---
+++ 5/shell.nix
@@ -0,0 +1,30 @@
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "default", doBenchmark ? false }:
+
+let
+
+  inherit (nixpkgs) pkgs;
+
+  f = { mkDerivation, base, stdenv, vector }:
+      mkDerivation {
+        pname = "a175";
+        version = "";
+        src = ./.;
+        isLibrary = false;
+        isExecutable = true;
+        executableHaskellDepends = [ base vector ];
+        doHaddock = false;
+        doCheck = false;
+        license = stdenv.lib.licenses.unfree;
+      };
+
+  haskellPackages = if compiler == "default"
+                       then pkgs.haskellPackages
+                       else pkgs.haskell.packages.${compiler};
+
+  variant = if doBenchmark then pkgs.haskell.lib.doBenchmark else pkgs.lib.id;
+
+  drv = variant (haskellPackages.callPackage f {});
+
+in
+
+  if pkgs.lib.inNixShell then drv.env else drv
